60af46a75814fc76735590196546b5572d5953b6
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2021-01-20 19:59:10 +0200

    refactor local_storage

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  // const [contacts, setContacts] = useState(() => {
-  //     return JSON.parse(localStorage.getItem('contacts')) ?? [...testContact];
-  // });
-
-  // useEffect(() => {
-  //     window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <div className="Phonebook">
       <h2 className="Phonebook_title">Phonebook</h2>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
+import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/contacts-reducer';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
 
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(persistConfig, contactsReducer),
   },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
 
-export default store;
+const persistor = persistStore(store);
 
-///
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// createStore(rootReducer, composeWithDevTools());
-//, combineReducers
+export default { persistor, store };
